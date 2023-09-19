dba5495c431738de5b997f1db6fe2af5349817b1
Milton Beltrán <76416426+MrRainbowDash@users.noreply.github.com>
2023-09-18 20:20:20 -0600

    Cambios

--- insert_signups.php
+++ insert_signups.php
@@ -4,10 +4,10 @@
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $email = $_POST['email'];
-$password = $_POST['password'];
+$password = $_POST['contrasena'];
 
 // Insert the data into the storage
-$sql = "INSERT INTO signups (first_name, last_name, email, password) VALUES ('$first_name', '$last_name', '$email')";
+$sql = "INSERT INTO signups (first_name, last_name, email, contrasena) VALUES ('$first_name', '$last_name', '$email', '$password')";
 mysqli_query($conn, $sql);
 
 // Redirect the user to the home page
