d880db9491f4bed68855f96bda56b3803e31d83f
Milton Beltrán <76416426+MrRainbowDash@users.noreply.github.com>
2023-09-18 20:18:22 -0600

    Clase summit

--- insert_signups.php
+++ insert_signups.php
@@ -4,9 +4,10 @@
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $email = $_POST['email'];
+$password = $_POST['password'];
 
 // Insert the data into the storage
-$sql = "INSERT INTO signups (first_name, last_name, email) VALUES ('$first_name', '$last_name', '$email')";
+$sql = "INSERT INTO signups (first_name, last_name, email, password) VALUES ('$first_name', '$last_name', '$email')";
 mysqli_query($conn, $sql);
 
 // Redirect the user to the home page
