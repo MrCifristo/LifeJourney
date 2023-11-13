6c5552dff52558243af1133c1fc6031f58d0f1a4
Milton Beltrán <miltonbeltran@ufm.edu>
2023-11-13 11:06:49 -0600

    DockerFile
    Cree el dockerfile y ya me levanta la imagen y el container.

--- public/apps.js
+++ public/apps.js
@@ -1,24 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const apiKey = 'TU_API_KEY'; 
-    const baseId = 'apphA4dkXJZdMjb68';
-    const tableName = 'shrFk9dYyfIZTO4aA'; 
-
-    const apiUrl = `https://api.airtable.com/v0/${baseId}/${tableName}`;
-
-    fetch(apiUrl, {
-        headers: {
-            Authorization: `Bearer ${apiKey}`
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        const datosElement = document.getElementById('datos');
-
-        data.records.forEach(record => {
-            const listItem = document.createElement('li');
-            listItem.textContent = record.fields.Nombre; 
-            datosElement.appendChild(listItem);
-        });
-    })
-    .catch(error => console.error('Error:', error));
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     const apiKey = 'TU_API_KEY';
+//     const baseId = 'apphA4dkXJZdMjb68';
+//     const tableName = 'shrFk9dYyfIZTO4aA';
+//
+//     const apiUrl = `https://api.airtable.com/v0/${baseId}/${tableName}`;
+//
+//     fetch(apiUrl, {
+//         headers: {
+//             Authorization: `Bearer ${apiKey}`
+//         }
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         const datosElement = document.getElementById('datos');
+//
+//         data.records.forEach(record => {
+//             const listItem = document.createElement('li');
+//             listItem.textContent = record.fields.Nombre;
+//             datosElement.appendChild(listItem);
+//         });
+//     })
+//     .catch(error => console.error('Error:', error));
+// })
