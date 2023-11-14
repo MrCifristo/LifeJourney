5f122e967a1010e0f2d71b5fd71e73fcfe5d4896
Milton Beltrán <miltonbeltran@ufm.edu>
2023-11-14 10:11:39 -0600

    le metimos los tests

--- public/apps.js
+++ public/apps.js
@@ -1,24 +1,33 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     const apiKey = 'TU_API_KEY';
-//     const baseId = 'apphA4dkXJZdMjb68';
-//     const tableName = 'shrFk9dYyfIZTO4aA';
-//
-//     const apiUrl = `https://api.airtable.com/v0/${baseId}/${tableName}`;
-//
-//     fetch(apiUrl, {
-//         headers: {
-//             Authorization: `Bearer ${apiKey}`
-//         }
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//         const datosElement = document.getElementById('datos');
-//
-//         data.records.forEach(record => {
-//             const listItem = document.createElement('li');
-//             listItem.textContent = record.fields.Nombre;
-//             datosElement.appendChild(listItem);
-//         });
-//     })
-//     .catch(error => console.error('Error:', error));
-// })
+function cargarDatosDeAirtable(apiKey, baseId, tableName, callback) {
+    const apiUrl = `https://api.airtable.com/v0/${baseId}/${tableName}`;
+
+    fetch(apiUrl, {
+        headers: {
+            Authorization: `Bearer ${apiKey}`
+        }
+    })
+        .then(response => response.json())
+        .then(data => callback(data))
+        .catch(error => console.error('Error:', error));
+}
+
+// Para mantener el comportamiento actual cuando se carga la página
+document.addEventListener('DOMContentLoaded', function() {
+    const apiKey = 'TU_API_KEY'; // Reemplaza con tu API key real
+    const baseId = 'apphA4dkXJZdMjb68'; // Reemplaza con tu Base ID real
+    const tableName = 'shrFk9dYyfIZTO4aA'; // Reemplaza con tu Table Name real
+
+    cargarDatosDeAirtable(apiKey, baseId, tableName, (data) => {
+        const datosElement = document.getElementById('datos');
+        data.records.forEach(record => {
+            const listItem = document.createElement('li');
+            listItem.textContent = record.fields.Nombre; // Asumiendo que 'Nombre' es uno de los campos en tu tabla de Airtable
+            datosElement.appendChild(listItem);
+        });
+    });
+});
+
+// Exportar para pruebas
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = cargarDatosDeAirtable;
+}
